21fd19a991951130d0f62cf8b39dd5a6481995fa
Colin Conn <conncf@mail.uc.edu>
2023-04-17 11:46:48 -0400

    push to work on laptop

---
+++ js/forceDirectedGraph.js
@@ -0,0 +1,82 @@
+class ForceDirectedGraph {
+    constructor(_config, _data) {
+        this.config = {
+            parentElement: _config.parentElement,
+            containerHeight: _config.containerHeight || 500,
+            containerWidth: _config.containerWidth || 140,
+            margin: {top: 40, right: 20, bottom: 30, left: 5},
+            toolTipPadding: _config.toolTipPadding || 15,
+        }
+
+        this.data = _data
+
+        this.initVis()
+    }
+
+    initVis() {
+        let vis = this;
+        
+        vis.width = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right
+        vis.height = vis.config.containerHeight - vis.config.margin.top - vis.config.margin.bottom
+
+        vis.chart = d3.select(vis.config.parentElement)
+            .attr('width', vis.config.containerWidth )
+            .attr('height', vis.config.containerHeight)
+            .append('g')
+                .attr('transform', `translate(${vis.config.margin.left},${vis.config.margin.top})`);
+    }
+
+    updateVis() {
+        let vis = this;
+
+        vis.simulation = d3.forceSimulation(vis.data.nodes)
+            .force('charge', d3.forceManyBody().strength(-20))
+            .force('center', d3.forceCenter(vis.width / 2, vis.height / 2))
+            .on('tick', ticked);
+
+        vis.nodeElements = vis.chart.append('g')
+            .selectAll('circle')
+            .data(vis.data.nodes)
+            .join('circle')
+              .attr('r', 5)
+
+        vis.textElements = vis.chart.append('g')
+            .selectAll('text')
+            .data(vis.data.nodes)
+            .join('text')
+            .text(n => n.label)
+                .attr('font-size', 15)
+                .attr('dx', 15)
+                .attr('dy', 4)
+
+        vis.simulation.force('link', d3.forceLink().id(l => l.id).strength(l => l.strength / 1000))
+
+        vis.linkElements = vis.chart.append('g')
+            .selectAll('line')
+            .data(vis.data.links)
+            .join('line')
+            .attr('stroke-width', 1)
+            .attr('stroke', '#E5E5E5')
+
+        vis.simulation.force('link').links(vis.data.links)
+
+        function ticked() {
+            vis.nodeElements
+                .attr('cx', d => d.x)
+                .attr('cy', d => d.y);
+
+            vis.textElements
+                .attr('x', n => n.x)
+                .attr('y', n => n.y)
+
+            vis.linkElements
+                .attr('x1', l => l.source.x)
+                .attr('y1', l => l.source.y)
+                .attr('x2', l => l.target.x)
+                .attr('y2', l => l.target.y)
+        }
+        
+
+
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,6 +1,76 @@
 let data
 
-d3.tsv('data/adventure_time_all_eps.tsv')
+d3.tsv('data/adventure_time_all_eps_with_scene_num.tsv')
     .then(_data => {
+        data = _data
+
+        data.forEach(d => {
+            d.seasonEpisodeScene = d.season + "-" + d.ep_num + "-" + d.scene_num
+        });
+        data.scenes = [...new Map(data.map(item => [item.seasonEpisodeScene,
+            item.chars_in_scene.split(', ')
+                .filter(c => !c.includes('&') 
+                    && !c.toLowerCase().includes(' and '))
+                .map(d => d.replace(/\[.*?\]/, '').replace("\""))]))]
+
+        //need list of characters in more than one scene
+        data.characters = data.scenes.map(i => i[1]).flat()
+        let characterFreq = []
+
+        for(character of data.characters){
+            if(characterFreq[0] == getId(character))
+                characterFreq.indexOf(getId(character))++
+            else
+                characterFreq[getId(character)] = 1
+        }
         
+        data.links = []
+        data.nodes = []
+
+
+        data.scenes.forEach(d => {
+            for (target of d[1]) {
+                if(data.nodes.filter(n => getId(n.label) == getId(target)).length < 1) {
+                    data.nodes.push({id: getId(target), label: target})
+                }
+                for(source of d[1]) {
+                    if(source != target) {
+                        if(data.links.some(l => l.target == getId(target) && l.source == getId(source)))
+                        {
+                            data.links.filter(l => (l.target == target && l.source == source))[0].strength++
+                        }
+                        else if(data.links.some(l => (l.target == getId(source) && l.source == getId(target))).length == 1)
+                        {
+                            console.log("foo")
+                            continue
+                        }
+                        else 
+                        {
+                            data.links.push({ target: getId(target), source: getId(source), strength: 1})
+                        }
+                    }
+                }
+            }
+        })
+        
+    console.log(data.links.filter(l => l.strength > 1))
+
+        // let forceDirectedGraph = new ForceDirectedGraph({
+		// 	parentElement: '#force-directed-graph',
+		// 	'containerHeight': 1000,
+		// 	'containerWidth': 1000
+		// }, data)
+		// forceDirectedGraph.updateVis()
     })
+
+    let getId = (d) => d.toLowerCase().replace(/\s+/g, '')
+
+    function processCharacters(character) {
+        switch(character){
+            case "simon":
+            case "simon":
+
+            default:
+                return character
+        }
+    }
