75108b60376327221f7024e5e0f464736158e215
Colin Conn <conncf@mail.uc.edu>
2023-04-19 15:54:08 -0400

    Added Comments

--- js/forceDirectedGraph.js
+++ js/forceDirectedGraph.js
@@ -29,29 +29,42 @@ class ForceDirectedGraph {
     updateVis() {
         let vis = this;
 
+        // Create simulation
+        // Force Many Body: Makes elements attract(+) or repel(-) each other
+        // Force center: Sets center of gravity
+        // Force link: Sets link force based on strength
+        // On Tick: updates element position each tick (gives the fun animation)
         vis.simulation = d3.forceSimulation(vis.data.nodes)
-            .force('charge', d3.forceManyBody().strength(-20))
+            .force('charge', d3.forceManyBody().strength(-800))
             .force('center', d3.forceCenter(vis.width / 2, vis.height / 2))
+            .force('link', d3.forceLink().id(l => l.id).strength(l => l.strength))
             .on('tick', ticked);
 
-        vis.nodeElements = vis.chart.append('g')
-            .selectAll('circle')
-            .data(vis.data.nodes)
-            .join('circle')
-              .attr('r', 5)
-              .attr('class', 'node')
+        // Applies links to the link force
+        vis.simulation.force('link').links(vis.data.links)
 
-        vis.textElements = vis.chart.append('g')
-            .selectAll('text')
-            .data(vis.data.nodes)
-            .join('text')
-            .text(n => n.label)
-                .attr('font-size', 15)
-                .attr('dx', 15)
-                .attr('dy', 4)
+        // Updates Element Positions
+        function ticked() {
 
-        vis.simulation.force('link', d3.forceLink().id(l => l.id).strength(l => l.strength))
+            // Node Circle Positions
+            vis.nodeElements
+                .attr('cx', d => d.x)
+                .attr('cy', d => d.y);
 
+            // Text Label Positions
+            vis.textElements
+                .attr('x', n => n.x)
+                .attr('y', n => n.y)
+
+            // Link Line Positions
+            vis.linkElements
+                .attr('x1', l => l.source.x)
+                .attr('y1', l => l.source.y)
+                .attr('x2', l => l.target.x)
+                .attr('y2', l => l.target.y)
+        }
+
+        // Creates SVG for link lines
         vis.linkElements = vis.chart.append('g')
             .selectAll('line')
             .data(vis.data.links)
@@ -60,25 +73,26 @@ class ForceDirectedGraph {
             .attr('stroke', '#E5E5E5')
             .attr('class', 'link')
 
-        vis.simulation.force('link').links(vis.data.links)
+        // Creates SVG for node circles
+        vis.nodeElements = vis.chart.append('g')
+            .selectAll('circle')
+            .data(vis.data.nodes)
+            .join('circle')
+              .attr('r', 5)
+              .attr('class', 'node')
 
-        function ticked() {
-            vis.nodeElements
-                .attr('cx', d => d.x)
-                .attr('cy', d => d.y);
+        // Creates SVG for text labels
+        vis.textElements = vis.chart.append('g')
+            .selectAll('text')
+            .data(vis.data.nodes)
+            .join('text')
+            .text(n => n.label)
+                .attr('font-size', 15)
+                .attr('dx', 15)
+                .attr('dy', 4)
+    }
 
-            vis.textElements
-                .attr('x', n => n.x)
-                .attr('y', n => n.y)
-
-            vis.linkElements
-                .attr('x1', l => l.source.x)
-                .attr('y1', l => l.source.y)
-                .attr('x2', l => l.target.x)
-                .attr('y2', l => l.target.y)
-        }
-        
-
-
+    renderVis(){
+        // Nothing yet
     }
 }
--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 let data
-
+let nonCharacters = ["woman", "man","audience", "crowd", "alltheanimals", "voice", "unknownvoice", "malevoice", "episodeends.", "episodeends", "all", "everyoneelse", "everyone", "both", ""]
 d3.tsv('data/adventure_time_all_eps_with_scene_num.tsv')
     .then(_data => {
         data = _data
@@ -7,92 +7,257 @@ d3.tsv('data/adventure_time_all_eps_with_scene_num.tsv')
         data.forEach(d => {
             d.seasonEpisodeScene = d.season + "-" + d.ep_num + "-" + d.scene_num
         });
+
+        // Get list of scenes in format {season-episode-scene, characters in scene}
+        
         data.scenes = [...new Map(data.map(item => [item.seasonEpisodeScene,
             item.chars_in_scene.split(', ')
-                .filter(c => !c.includes('&') 
-                    && !c.toLowerCase().includes(' and '))
-                .map(d => d.replace(/\[.*?\]/, '').replace("\""))]))]
+                .filter(c => !(nonCharacters.includes(getId(c)) // filters out "all" and similar characters
+                    || c.includes('&') 
+                    || c.toLowerCase().includes(' and ') // filters out characters that are actually multiple speaking at once
+                    || c.toLowerCase().includes('and ') // filters out characters that are actually multiple speaking at once
+                    || c.toLowerCase().includes('/')))
+                // removes brackets and parentheses along with their contents, and removes quotes from names
+                // TODO: Removing quotes not working correctly
+                .map(d => processCharacters(d.replace(/\[.*?\]/, '').replace(/\(.*?\)/, '').replace('"', '')))]))]
 
-        //need list of characters in more than one scene
-        data.characters = data.scenes.map(i => i[1]).flat()
-        let characterFreq = []
-        let frequentCharacters = {}
+        data.characters = data.scenes.map(i => i[1]).flat() // List of all characters (not unique)
+        let characterFreq = [] //list of all characters and their counts
+        let frequentCharacters = {} // list of frequent characters (in more than one scene)
 
+        // loop through all characters and calculates their scene count
         for(character of data.characters){
             if(characterFreq[(getId(character))])
                 characterFreq[getId(character)]++
             else
                 characterFreq[getId(character)] = 1
         }
-
-        console.log("foo")
-
+        
+        // Remove characters in just one scene
         for (let character in characterFreq){
-            if(characterFreq[getId(character)] > 2)
+            if(characterFreq[getId(character)] > 1)
                 frequentCharacters[getId(character)] = characterFreq[getId(character)]
         }
 
-        frequentCharacters = Object.entries(frequentCharacters)
+        // Turning frequent characters into a list, sorting, and getting top 50
+        frequentCharacters = Object.entries(frequentCharacters).sort((a,b) => b[1] - a[1]).splice(0,50)
         
-        console.log(frequentCharacters)
         data.links = []
         data.nodes = []
 
+        // Create links and nodes
         data.scenes.forEach(d => {
+            // Loop through the frequent characters from the current scene to be a link target
             for (let target of d[1].filter(c => frequentCharacters.some(f => getId(f[0]) == getId(c)))){
-                let targetId = getId(target)
 
+                let targetId = getId(target) // Get id form of target
+
+                // If that character doesn't have a node yet, make one
                 if(!data.nodes.some(n => getId(n.label) == targetId)) {
                     data.nodes.push({id: targetId, label: target})
                 }
-                for(let source of d[1].filter(c => frequentCharacters.some(f => getId(f[0]) == getId(c)))) {
-                    let sourceId = getId(source)
 
-                    if(source != target) {
-                        if(data.links.some(l => l.target == targetId && l.source == sourceId))
-                        {
-                            data.links.filter(l => (l.target == targetId && l.source == sourceId))[0].strength += 1
-                        }
-                        else if(data.links.some(l => (l.target == sourceId && l.source == targetId)))
-                        {
-                            continue
-                        }
-                        else 
-                        {
-                            data.links.push({ target: targetId, source: sourceId, strength: 1 })
-                        }
-                    }
+                // Loop through the frequent characters from the current scene again (not including the target character) to be a link source
+                for(let source of d[1].filter(c => getId(c) != targetId && frequentCharacters.some(f => getId(f[0]) == getId(c)))) {
+
+                    let sourceId = getId(source) // get id form of source
+
+                    // If the two characters have an existing link, increment the strength
+                    if(data.links.some(l => l.target == targetId && l.source == sourceId))
+                        data.links.filter(l => (l.target == targetId && l.source == sourceId))[0].strength += 1
+
+                    // If the characters have a link in the opposite direction, skip (avoids double dipping links)
+                    else if(data.links.some(l => (l.target == sourceId && l.source == targetId)))
+                        continue
+
+                    // Otherwise create a new link
+                    else 
+                        data.links.push({ target: targetId, source: sourceId, strength: 1 })
                 }
             }
         })
 
-        console.log(data.links)
-
+        // Go through links and calculate actual strength (occurrences together / total scenes of source + target)
         data.links.forEach(d => {
-            if(d.strength == 1){
-                console.log(data.links.splice(data.links.indexOf(d), 1))
-                data.links.splice(data.links.indexOf(d), 1)
-            }
-            
-            d.strength = (1.0 / d.strength)
+            d.strength = d.strength / (characterFreq[d.source] + characterFreq[d.target])
         })
-        
-    console.log(data.links)
 
         let forceDirectedGraph = new ForceDirectedGraph({
 			parentElement: '#force-directed-graph',
-			'containerHeight': 1000,
-			'containerWidth': 1000
+			'containerHeight': 2000,
+			'containerWidth': 2000
 		}, data)
 		forceDirectedGraph.updateVis()
     })
 
     let getId = (d) => d.toLowerCase().replace(/\s+/g, '')
 
+    //Take in character, get it's id form and return it's actual character
     function processCharacters(character) {
-        switch(character){
+        //console.log(getId(character))
+        switch(getId(character)){
+            case "pen":
+            case "futurefinn":
+            case "pastfinn":
+                return "Finn"
+
+            case "jake'ssubconscious":
+            case "babyjake":
+                return "Jake"
+
             case "simon":
-            case "simon":
+            case "simonpetrikov":
+                return "Ice King"
+
+            case "princessbubblegum'svoice":
+            case "bubblegum":
+            case "youngbubblegum":
+            case "youngprincessbubblegum":
+            case "princess":
+            case "bonnie": 
+                return "Princess Bubblegum"
+
+            case "marcy":
+                return "Marceline"
+
+            case "lady":
+                return "Lady Rainicorn"
+
+            case "lsp":
+                return "Lumpy Space Princess"
+
+            case "normalman":
+                return "Magic Man"
+
+            case "gunther":
+            case "icething":
+                return "Gunter"
+            
+            case "lich":
+                return "The Lich"
+
+            case "starchie":
+                return "Starchy"
+
+            case "t.v.":
+                return "TV"
+
+            case "vampireking":
+            case "hunson":
+                return "Hunson Abadeer"
+            
+            case "earloflemongrab":
+            case "lemongrabclone":
+            case "lemongrab":
+            case "lemongrab2":
+            case "lemongrab3":
+                return "Lemongrabs"
+
+            case "holo-margaret":
+                return "Margaret"
+            
+            case "fernface":
+                return "Fern"
+                
+            case "gob":
+            case "glob":
+            case "grob":
+            case "grod":
+                return "Grob Gob Glob Grod"
+            
+            case "mrpig":
+                return "Mr. Pig"
+
+            case "unclegumbald":
+                return "Gumbald"
+
+            case "fionna\"":
+                return "Fionna"
+
+            case "dirtbeerguy":
+                return "Root Beer Guy"
+
+            case "hotdogknight#1":
+            case "hotdogknight#2":
+            case "hotdogknight#3":
+            case "hotdogknight#4":
+                return "Hot Dog Knights"
+
+            case "goblinguard1":
+            case "goblinguard2":
+            case "goblin":
+                return "Goblins"
+            
+            case "gnomeruler":
+                return "Gnomes"
+
+            case "long-hairednymph":
+            case "short-hairednymph":
+            case "nymph":
+                return "Nymphs"
+
+            case "gumballguardian":
+                return "Gumball Guardians"
+
+            case "bananaguard#1":
+            case "bananaguard#2":
+            case "anotherbananaguard":
+            case "bananaguard":
+            case "bananaguard1":
+            case "bananaguard2":
+            case "unidentifiablebananaguard":
+                return "Banana Guards"
+
+            case "headmarauder":
+            case "marauder":
+                return "Marauders"
+
+            case "candyperson":
+            case "candyperson1":
+            case "candyperson2":
+            case "candyperson#29":
+            case "candyperson#1":
+                return "Candy People"
+
+            case "red-tiebusinessman":
+                return "Businessmen"
+
+            case "oneballoon":
+                return "Balloons"
+
+            case "penguin":
+                return "Penguins"
+
+            case "flameguard":
+                return "Flame Guards"
+
+            case "greenlollipopgirl":
+                return "Lollipop Girl"
+
+            case "moldo":
+                return "Moldos"
+            
+            case "headclownnurse":
+            case "bigclownnurse":
+                return "Clown Nurses"
+
+            case "fatvillager":
+            case "fatvillagers":
+            case "villager":
+                return "Villagers"
+
+            case "candychild2":
+            case "candychild3":
+            case "candychild":
+                return "Candy Children"
+
+            case "dmo":
+            case "smo1":
+            case "smo3":
+            case "smo4":
+            case "mo":
+            case "allmo":
+                return "MOs"
 
             default:
                 return character
